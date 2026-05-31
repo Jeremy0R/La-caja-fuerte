3eb5f1f8576a7e036351693bfd49f6aaf3007179
Jeremy <jeremyositoxs@gmail.com>
2026-05-31 15:48:02 -0600

    Configuración de splash screen para inicio de la aplicación

--- app/src/main/java/com/example/lacajafuerte/MainActivity.java
+++ app/src/main/java/com/example/lacajafuerte/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.lacajafuerte;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // 1. Enlazamos el ImageView del diseño
+        ImageView ivSplashGif = findViewById(R.id.ivSplashGif);
+
+        // 2. Cargamos el GIF usando Glide
+        Glide.with(this)
+                .load(R.drawable.cfsplash)
+                .into(ivSplashGif);
+
+        // 3. Temporizador de 3 segundos para pasar al Menú
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // Saltamos a MenuActivity (la crearemos a continuación)
+            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
     }
 }
