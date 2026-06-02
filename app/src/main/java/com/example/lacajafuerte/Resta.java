bcf07781f83bbce1ada542fc0d08505e56bbdeda
Jeremy <jeremyositoxs@gmail.com>
2026-06-01 21:05:02 -0600

    Se agregaron las clases para suma, resta, mul y div

---
+++ app/src/main/java/com/example/lacajafuerte/Resta.java
@@ -0,0 +1,30 @@
+package com.example.lacajafuerte;
+
+import java.util.Random;
+
+public class Resta extends OperacionMatematica {
+
+    public Resta(int nivelDificultad) {
+        super(nivelDificultad);
+    }
+
+    @Override
+    public void generarOperacion() {
+        Random random = new Random();
+        int limite = nivelDificultad * 10;
+
+        int temp1 = random.nextInt(limite) + 1;
+        int temp2 = random.nextInt(limite) + 1;
+
+        // El mayor siempre va primero para no tener resultados negativos
+        num1 = Math.max(temp1, temp2);
+        num2 = Math.min(temp1, temp2);
+
+        respuestaCorrecta = num1 - num2;
+    }
+
+    @Override
+    public String getAcertijoComoTexto() {
+        return num1 + " - " + num2 + " = ?";
+    }
+}
